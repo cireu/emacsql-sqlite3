f7f515e1b19e0d3eb1ee7a3b028a9d99e8776eb8
Zhu Zihao <all_but_last@163.com>
2019-09-12 12:42:12 +0800

    enh(test): Simplify unit test. (#5)

--- emacsql-sqlite3-test.el
+++ emacsql-sqlite3-test.el
@@ -8,86 +8,76 @@
 (defvar emacsql-tests-timeout 4
   "Be aggressive about not waiting on subprocesses in unit tests.")
 
-(defvar emacsql-tests-connection-factories
-  (let (factories)
-    (cl-labels ((reg (name &rest args)
-                  (push (cons name (apply #'apply-partially args)) factories)))
-      (reg "sqlite3" #'emacsql-sqlite3 nil))
-    (nreverse factories))
-  "List of connection factories to use in unit tests.")
+(defun emacsql-make-connection ()
+  (emacsql-sqlite3 nil))
 
 (ert-deftest emacsql-basic ()
   "A short test that fully interacts with SQLite."
   (let ((emacsql-global-timeout emacsql-tests-timeout))
-    (dolist (factory emacsql-tests-connection-factories)
-      (emacsql-with-connection (db (funcall (cdr factory)))
-        (emacsql db [:create-temporary-table foo ([x])])
-        (should-error (emacsql db [:create-temporary-table foo ([x])]))
-        (emacsql db [:insert :into foo :values ([1] [2] [3])])
-        (should (equal (emacsql db [:select * :from foo])
-                       '((1) (2) (3))))))))
+    (emacsql-with-connection (db (emacsql-make-connection))
+      (emacsql db [:create-temporary-table foo ([x])])
+      (should-error (emacsql db [:create-temporary-table foo ([x])]))
+      (emacsql db [:insert :into foo :values ([1] [2] [3])])
+      (should (equal (emacsql db [:select * :from foo])
+                     '((1) (2) (3)))))))
 
 (ert-deftest emacsql-nul-character ()
   "Try inserting and retrieving strings with a NUL byte."
   (let ((emacsql-global-timeout emacsql-tests-timeout))
-    (dolist (factory emacsql-tests-connection-factories)
-      (emacsql-with-connection (db (funcall (cdr factory)))
-        (emacsql db [:create-temporary-table foo ([x])])
-        (emacsql db [:insert :into foo :values (["a\0bc"])])
-        (should (equal (emacsql db [:select * :from foo])
-                       '(("a\0bc"))))))))
+    (emacsql-with-connection (db (emacsql-make-connection))
+      (emacsql db [:create-temporary-table foo ([x])])
+      (emacsql db [:insert :into foo :values (["a\0bc"])])
+      (should (equal (emacsql db [:select * :from foo])
+                     '(("a\0bc")))))))
 
 (ert-deftest emacsql-foreign-key ()
   "Tests that foreign keys work properly through EmacSQL."
   (let ((emacsql-global-timeout emacsql-tests-timeout))
-    (dolist (factory emacsql-tests-connection-factories)
-      (emacsql-with-connection (db (funcall (cdr factory)))
-        (unwind-protect
-             (progn
-               ;; Temporarily open foreign key support.
-               (emacsql db [:pragma (= foreign_keys ON)])
-               (emacsql-thread db
-                 [:create-table person ([(id integer :primary-key) name])]
-                 [:create-table likes
-                  ([(personid integer) color]
-                   (:foreign-key [personid] :references person [id]
-                                 :on-delete :cascade))]
-                 [:insert :into person :values ([0 "Chris"] [1 "Brian"])])
-               (should (equal (emacsql db [:select * :from person :order-by id])
-                              '((0 "Chris") (1 "Brian"))))
-               (emacsql db [:insert :into likes
-                            :values ([0 red] [0 yellow] [1 yellow])])
-               (should (equal (emacsql db [:select * :from likes
-                                           :order-by [personid color]])
-                              '((0 red) (0 yellow) (1 yellow))))
-               (emacsql db [:delete :from person :where (= id 0)])
-               (should (equal (emacsql db [:select * :from likes])
-                              '((1 yellow)))))
-          (emacsql-thread db
-            [:drop-table likes]
-            [:drop-table person]))))))
+    (emacsql-with-connection (db (emacsql-make-connection))
+      (unwind-protect
+           (progn
+             ;; Temporarily open foreign key support.
+             (emacsql db [:pragma (= foreign_keys ON)])
+             (emacsql-thread db
+               [:create-table person ([(id integer :primary-key) name])]
+               [:create-table likes
+                ([(personid integer) color]
+                 (:foreign-key [personid] :references person [id]
+                               :on-delete :cascade))]
+               [:insert :into person :values ([0 "Chris"] [1 "Brian"])])
+             (should (equal (emacsql db [:select * :from person :order-by id])
+                            '((0 "Chris") (1 "Brian"))))
+             (emacsql db [:insert :into likes
+                          :values ([0 red] [0 yellow] [1 yellow])])
+             (should (equal (emacsql db [:select * :from likes
+                                         :order-by [personid color]])
+                            '((0 red) (0 yellow) (1 yellow))))
+             (emacsql db [:delete :from person :where (= id 0)])
+             (should (equal (emacsql db [:select * :from likes])
+                            '((1 yellow)))))
+        (emacsql-thread db
+          [:drop-table likes]
+          [:drop-table person])))))
 
 (ert-deftest emacsql-error ()
   "Check that we're getting expected conditions."
   (should-error (emacsql-compile nil [:insert :into foo :values 1])
                 :type 'emacsql-syntax)
   (let ((emacsql-global-timeout emacsql-tests-timeout))
-    (dolist (factory emacsql-tests-connection-factories)
-      (emacsql-with-connection (db (funcall (cdr factory)))
-        (emacsql db [:create-temporary-table foo ([x])])
-        (should-error (emacsql db [:create-temporary-table foo ([x])])
-                      :type 'emacsql-error)))))
+    (emacsql-with-connection (db (emacsql-make-connection))
+      (emacsql db [:create-temporary-table foo ([x])])
+      (should-error (emacsql db [:create-temporary-table foo ([x])])
+                    :type 'emacsql-error))))
 
 (ert-deftest emacsql-special-chars ()
   "A short test that interacts with SQLite with special characters."
   (let ((emacsql-global-timeout 4))
-    (dolist (factory emacsql-tests-connection-factories)
-      (emacsql-with-connection (db (funcall (cdr factory)))
-        (emacsql db [:create-temporary-table test-table ([x])])
-        (emacsql db [:insert-into test-table :values ([""] [\] ["m\nn"])])
-        (should (process-live-p (emacsql-process db)))
-        (should (equal (emacsql db [:select * :from test-table])
-                       '(("") (\) ("m\nn"))))))))
+    (emacsql-with-connection (db (emacsql-make-connection))
+      (emacsql db [:create-temporary-table test-table ([x])])
+      (emacsql db [:insert-into test-table :values ([""] [\] ["m\nn"])])
+      (should (process-live-p (emacsql-process db)))
+      (should (equal (emacsql db [:select * :from test-table])
+                     '(("") (\) ("m\nn")))))))
 
 (defun emacsql-sqlite3-run-dot-command-sync (conn cmd &rest args)
   "Like `emacsql-sqlite3-run-dot-command', but synchronously."
@@ -102,19 +92,18 @@
 
 (ert-deftest emacsql-sqlite3-dot-command ()
   "Test `emacsql-sqlite3-run-dot-command'."
-  (dolist (factory emacsql-tests-connection-factories)
-    (emacsql-with-connection (conn (funcall (cdr factory)))
-      (should (equal
-               (emacsql-sqlite3-run-dot-command-sync conn :print "OK")
-               "OK\n"))
-      (should (equal
-               (emacsql-sqlite3-run-dot-command-sync conn 'print "OK")
-               "OK\n"))
-      (should (equal
-               (emacsql-sqlite3-run-dot-command-sync conn "print" "OK")
-               "OK\n"))
-      (should (equal
-               (emacsql-sqlite3-run-dot-command-sync conn "print" "O K")
-               "O K\n")))))
+  (emacsql-with-connection (conn (emacsql-make-connection))
+    (should (equal
+             (emacsql-sqlite3-run-dot-command-sync conn :print "OK")
+             "OK\n"))
+    (should (equal
+             (emacsql-sqlite3-run-dot-command-sync conn 'print "OK")
+             "OK\n"))
+    (should (equal
+             (emacsql-sqlite3-run-dot-command-sync conn "print" "OK")
+             "OK\n"))
+    (should (equal
+             (emacsql-sqlite3-run-dot-command-sync conn "print" "O K")
+             "O K\n"))))
 
 ;;; emacsql-sqlite3-tests.el ends here
